bea91a783a30e68026d333dea4c21dce7832ad07
stevenrchun <stevenrchun@users.noreply.github.com>
2025-05-17 22:50:22 +0000

    deploy: 830d392836082e4523e9023aedc00c9bfa7b60af

--- eleventy.config.js
+++
@@ -1,5 +0,0 @@
-export default function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("style.css");
-  eleventyConfig.addPassthroughCopy("assets");
-  //eleventyConfig.addWatchTarget("style.css");
-}
