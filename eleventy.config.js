5f376442a2bae89abd68311881a4c727bba15ac6
Steven Chun <steven@Stevens-MacBook-Pro.local>
2025-05-17 15:54:56 -0700

    copy CNAME to build

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,6 @@
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("CNAME");
   //eleventyConfig.addWatchTarget("style.css");
 }
