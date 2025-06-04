d5c770db034245f5d976610cbcce9f26f6fa55df
Steven Chun <steven@Stevens-MacBook-Pro.local>
2025-06-04 00:14:58 -0700

    add lodging page

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,7 @@ export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("itinerary.css");
   eleventyConfig.addPassthroughCopy("travel.css");
+  eleventyConfig.addPassthroughCopy("lodging.css");
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("CNAME");
   eleventyConfig.setLiquidOptions({
