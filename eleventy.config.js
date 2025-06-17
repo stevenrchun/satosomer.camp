e0b826375dd46e4e9c8012db33a6aca9077e2e96
Steven Chun <steven@Stevens-MacBook-Pro.local>
2025-06-17 00:13:25 -0700

    packing page

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,7 @@ export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("itinerary.css");
   eleventyConfig.addPassthroughCopy("travel.css");
   eleventyConfig.addPassthroughCopy("lodging.css");
+  eleventyConfig.addPassthroughCopy("packing.css");
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("CNAME");
   eleventyConfig.setLiquidOptions({
